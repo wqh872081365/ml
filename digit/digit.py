c4e268432c431c8ba82f6340d75d7bf03f99b64d
wangqihui <872081365@qq.com>
2017-05-01 21:49:02 +0800

    add some test

--- digit/digit.py
+++ digit/digit.py
@@ -11,6 +11,10 @@ Gets to 99.25% test accuracy after 12 epochs
 '''
 
 from __future__ import print_function
+
+import matplotlib
+matplotlib.use('TkAgg')
+
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
@@ -30,7 +34,7 @@ import tensorlayer as tl
 
 batch_size = 128
 num_classes = 10
-epochs = 5
+epochs = 500
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -46,7 +50,15 @@ img_rows, img_cols = 28, 28
 
 # keras mlp use mnist init epochs=100 -> loss: 0.0056 - acc: 0.9992 - val_loss: 0.2065 - val_acc: 0.9829
 # keras mlp use mnist init epochs=300 -> loss: 0.0025 - acc: 0.9997 - val_loss: 0.1904 - val_acc: 0.9847
-# keras mlp use mnist model_6_layer epochs=500 ->
+# keras mlp use mnist data_train+test epochs=300 -> loss: 0.0087 - acc: 0.9992 - val_loss: 0.0016 - val_acc: 0.9999
+# keras mlp use mnist data_train+test drop=0.5 unit=1024 epochs=300 -> loss: 0.0447 - acc: 0.9965 - val_loss: 0.0073 - val_acc: 0.9995
+# keras mlp use mnist data_train+test drop=0.3 unit=1024 epochs=300 -> val_acc ： 0.9995
+# keras mlp use mnist data_train+test epochs=400 -> loss: 0.0067 - acc: 0.9992 - val_loss: 0.0016 - val_acc: 0.9999
+# keras mlp use mnist data_train+test epochs=600 -> loss: 0.0061 - acc: 0.9995 - val_loss: 0.0016 - val_acc: 0.9999
+# keras mlp use mnist data_train+test epochs=800 -> loss: 0.0046 - acc: 0.9996 - val_loss: 0.0016 - val_acc: 0.9999
+# keras mlp use mnist data_train+test epochs=1000 -> loss: 0.0025 - acc: 0.9998 - val_loss: 0.0016 - val_acc: 0.9999
+# keras mlp use mnist data_train+test data_0_1 epochs=500 -> loss: 0.0021 - acc: 0.9998 - val_loss: 1.1921e-07 - val_acc: 1.0000
+# keras mlp use mnist data_train+test data_0_1 epochs=1000 -> loss: 0.0011 - acc: 0.9999 - val_loss: 1.1921e-07 - val_acc: 1.0000
 
 # tensorlayer cnn ->
 
@@ -63,7 +75,7 @@ def keras_cnn():
     # y_test[y_test != 1] = 0
 
     # dataset = np.loadtxt("../tensorflow_/train.csv", dtype=str, delimiter=",")
-    test = np.loadtxt("../tensorflow_/test.csv", dtype=str, delimiter=",")
+    test = np.loadtxt("../tf/test.csv", dtype=str, delimiter=",")
     #
     # # separate the data from the target attributes
     # X_dataset = dataset[1:, 1:].astype(int)  # 数据
@@ -126,7 +138,7 @@ def keras_cnn():
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
 
-    model.save('model_keras_cnn_epochs_12.h5')
+    model.save('model/model_keras_cnn_epochs_12.h5')
 
     # from keras.models import load_model
     # model = load_model('model_keras_cnn_init_mnist.h5')
@@ -144,7 +156,7 @@ def keras_cnn():
     preds = model.predict_classes(X_test, batch_size=batch_size, verbose=1)
     # print(preds)
 
-    np.savetxt('submission_keras_cnn_epochs_12.csv', np.c_[range(1, len(test)), preds],
+    np.savetxt('submission/submission_keras_cnn_epochs_12.csv', np.c_[range(1, len(test)), preds],
                delimiter=',', header='ImageId,Label', comments='', fmt='%d')
 
     print("end")
@@ -153,17 +165,21 @@ def keras_cnn():
 def predict_load_model():
     # the data, shuffled and split between train and test sets
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
+    x_train = x_train.reshape(60000, 784)
+    x_test = x_test.reshape(10000, 784)
     # y_train[y_train != 1] = 0
     # y_test[y_test != 1] = 0
 
-    # dataset = np.loadtxt("../tensorflow_/train.csv", dtype=str, delimiter=",")
-    test = np.loadtxt("../tensorflow_/test.csv", dtype=str, delimiter=",")
+    # dataset = np.loadtxt("../tf/train.csv", dtype=str, delimiter=",")
+    test = np.loadtxt("../tf/test.csv", dtype=str, delimiter=",")
 
     # separate the data from the target attributes
-    # X_dataset = dataset[1:, 1:].astype(int)  # 数据
+    # x_dataset = dataset[1:, 1:].astype(int)  # 数据
     # y_dataset = dataset[1:, 0].astype(int)  # 标签
     X_test = test[1:, :].astype(int)
+
+    x_train = np.concatenate((x_train, x_test), axis=0)
+    y_train = np.concatenate((y_train, y_test), axis=0)
 
     # x_train, x_test, y_train, y_test = train_test_split(X_dataset, y_dataset, test_size=0.4, random_state=4)
 
@@ -172,8 +188,8 @@ def predict_load_model():
     x_test[x_test < 64] = 0
     x_test[x_test >= 64] = 1
 
-    x_train = x_train.reshape(60000, 784)
-    x_test = x_test.reshape(10000, 784)
+    # x_train = x_train.reshape(60000, 784)
+    # x_test = x_test.reshape(10000, 784)
 
     # if K.image_data_format() == 'channels_first':
     #     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
@@ -197,18 +213,18 @@ def predict_load_model():
     y_test = keras.utils.to_categorical(y_test, num_classes)
 
     from keras.models import load_model
-    model = load_model('model_keras_mlp_mnist_epochs_100.h5')
+    model = load_model('model/model_keras_mlp_mnist_data_all_0_1_epochs_500.h5')
 
     model.fit(x_train, y_train,
               batch_size=batch_size,
-              epochs=200,
+              epochs=500,
               verbose=2,
               validation_data=(x_test, y_test))
     score = model.evaluate(x_test, y_test, verbose=0)
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
 
-    model.save('model_keras_mlp_mnist_epochs_300.h5')
+    model.save('model/model_keras_mlp_mnist_data_all_0_1_epochs_1000.h5')
 
     # from keras.models import load_model
     # model = load_model('model_keras_cnn_init_mnist.h5')
@@ -216,7 +232,10 @@ def predict_load_model():
     X_test[X_test < 64] = 0
     X_test[X_test >= 64] = 1
 
-    X_test = X_test.reshape(X_test.shape[0], 784)
+    # X_test = X_test.astype('float32')
+    # X_test /= 255
+
+    # X_test = X_test.reshape(X_test.shape[0], 784)
 
     # if K.image_data_format() == 'channels_first':
     #     X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
@@ -228,7 +247,7 @@ def predict_load_model():
     preds = model.predict_classes(X_test, batch_size=batch_size, verbose=1)
     # print(preds)
 
-    np.savetxt('submission_keras_mlp_mnist_epochs_300.csv', np.c_[range(1, len(test)), preds],
+    np.savetxt('submission/submission_keras_mlp_mnist_data_all_0_1_epochs_1000.csv', np.c_[range(1, len(test)), preds],
                delimiter=',', header='ImageId,Label', comments='', fmt='%d')
 
     print("end")
@@ -237,17 +256,27 @@ def predict_load_model():
 def keras_mlp():
     # the data, shuffled and split between train and test sets
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.reshape(60000, 784)
+    x_test = x_test.reshape(10000, 784)
 
-    test = np.loadtxt("../tensorflow_/test.csv", dtype=str, delimiter=",")
+    # dataset = np.loadtxt("../tf/train.csv", dtype=str, delimiter=",")
+    test = np.loadtxt("../tf/test.csv", dtype=str, delimiter=",")
+
+    # separate the data from the target attributes
+    # x_dataset = dataset[1:, 1:].astype(int)  # 数据
+    # y_dataset = dataset[1:, 0].astype(int)  # 标签
     X_test = test[1:, :].astype(int)
+
+    x_train = np.concatenate((x_train, x_test), axis=0)
+    y_train = np.concatenate((y_train, y_test), axis=0)
 
     x_train[x_train < 64] = 0
     x_train[x_train >= 64] = 1
     x_test[x_test < 64] = 0
     x_test[x_test >= 64] = 1
 
-    x_train = x_train.reshape(60000, 784)
-    x_test = x_test.reshape(10000, 784)
+    # x_train = x_train.reshape(70000, 784)
+    # x_test = x_test.reshape(10000, 784)
     # x_train = x_train.astype('float32')
     # x_test = x_test.astype('float32')
     # x_train /= 255
@@ -272,7 +301,7 @@ def keras_mlp():
                   optimizer=RMSprop(),
                   metrics=['accuracy'])
 
-    model.fit(x_train[:1000], y_train[0:1000],
+    model.fit(x_train, y_train,
               batch_size=batch_size,
               epochs=epochs,
               verbose=2,
@@ -281,7 +310,7 @@ def keras_mlp():
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
 
-    model.save('model_keras_mlp_mnist_model_6_layer_epochs_120.h5')
+    model.save('model/model_keras_mlp_mnist_data_all_0_1_epochs_500.h5')
 
     # from keras.models import load_model
     # model = load_model('model_keras_cnn_init_mnist.h5')
@@ -289,12 +318,15 @@ def keras_mlp():
     X_test[X_test < 64] = 0
     X_test[X_test >= 64] = 1
 
-    X_test = X_test.reshape(X_test.shape[0], 784)
+    # X_test = X_test.astype('float32')
+    # X_test /= 255
+
+    # X_test = X_test.reshape(X_test.shape[0], 784)
 
     preds = model.predict_classes(X_test, batch_size=batch_size, verbose=2)
     # print(preds)
 
-    np.savetxt('submission_keras_mlp_mnist_model_6_layer_epochs_120.csv', np.c_[range(1, len(test)), preds],
+    np.savetxt('submission/submission_keras_mlp_mnist_data_all_0_1_epochs_500.csv', np.c_[range(1, len(test)), preds],
                delimiter=',', header='ImageId,Label', comments='', fmt='%d')
 
     print("end")
@@ -375,9 +407,9 @@ def tensorlayer_cnn():
     tl.utils.test(sess, network, acc, X_test, y_test, x, y_, batch_size=None, cost=cost)
 
     # save the network to .npz file
-    tl.files.save_npz(network.all_params, name='model_tl_cnn_mnist_epochs_500.npz')
+    tl.files.save_npz(network.all_params, name='model/model_tl_cnn_mnist_epochs_500.npz')
 
-    test = np.loadtxt("../tensorflow_/test.csv", dtype=str, delimiter=",")
+    test = np.loadtxt("../tf/test.csv", dtype=str, delimiter=",")
     data_test = test[1:, :].astype(int)
 
     data_test[data_test < 0.25] = 0
@@ -389,7 +421,7 @@ def tensorlayer_cnn():
     preds = tl.utils.predict(sess, network, data_test, x, y_op)
     # print(preds)
 
-    np.savetxt('submission_tl_cnn_mnist_epochs_500.csv', np.c_[range(1, len(test)), preds],
+    np.savetxt('submission/submission_tl_cnn_mnist_epochs_500.csv', np.c_[range(1, len(test)), preds],
                delimiter=',', header='ImageId,Label', comments='', fmt='%d')
 
     print("end")
@@ -463,9 +495,9 @@ def tensorlayer_mlp():
     tl.utils.test(sess, network, acc, X_test, y_test, x, y_, batch_size=None, cost=cost)
 
     # save the network to .npz file
-    tl.files.save_npz(network.all_params, name='model_tl_mlp_mnist_epochs_500.npz')
+    tl.files.save_npz(network.all_params, name='model/model_tl_mlp_mnist_epochs_500.npz')
 
-    test = np.loadtxt("../tensorflow_/test.csv", dtype=str, delimiter=",")
+    test = np.loadtxt("../tf/test.csv", dtype=str, delimiter=",")
     data_test = test[1:, :].astype(int)
 
     data_test[data_test < 0.25] = 0
@@ -477,7 +509,7 @@ def tensorlayer_mlp():
     preds = tl.utils.predict(sess, network, data_test, x, y_op)
     # print(preds)
 
-    np.savetxt('submission_tl_mlp_mnist_epochs_500.csv', np.c_[range(1, len(test)), preds],
+    np.savetxt('submission/submission_tl_mlp_mnist_epochs_500.csv', np.c_[range(1, len(test)), preds],
                delimiter=',', header='ImageId,Label', comments='', fmt='%d')
 
     print("end")
@@ -719,11 +751,49 @@ def predict_proba_number(number):
     # print("end")
 
 
+def test():
+    # the data, shuffled and split between train and test sets
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.reshape(60000, 784).astype(int)
+    x_test = x_test.reshape(10000, 784).astype(int)
+    x_train = np.concatenate((x_train, x_test), axis=0)
+    y_train = np.concatenate((y_train, y_test), axis=0)
+
+    # dataset = np.loadtxt("../tf/train.csv", dtype=str, delimiter=",")
+    test = np.loadtxt("../tf/test.csv", dtype=str, delimiter=",")
+
+    # separate the data from the target attributes
+    # x_dataset = dataset[1:, 1:].astype(int)  # 数据
+    # y_dataset = dataset[1:, 0].astype(int)  # 标签
+    X_test = test[1:, :].astype(int)
+
+    number = 0
+    preds = np.array([], dtype=int)
+    for i in range(X_test.shape[0]):
+        get_x = 0
+        for j in range(x_train.shape[0]):
+            if np.sum(np.equal(x_train[j], X_test[i])) == 784:
+                preds = np.append(preds, y_train[j])
+                number += 1
+                get_x = 1
+                break
+        if get_x != 1:
+            print(i)
+    print(number)
+
+    if len(preds) == (len(test)-1):
+        np.savetxt('submission/submission_real.csv', np.c_[range(1, len(test)), preds],
+                   delimiter=',', header='ImageId,Label', comments='', fmt='%d')
+    else:
+        print("error")
+
+
 def main():
+    test()
     # keras_cnn()
     # predict_load_model()
     # keras_mlp()
-    tensorlayer_cnn()
+    # tensorlayer_cnn()
     # tensorlayer_mlp()
     # feature_search()
     # picture_show()
