51e80f4ce5fb6780ca6adf5d00eaec1058ca502a
wangqihui <872081365@qq.com>
2017-05-02 08:45:01 +0800

    add rainforest test

--- rainforest/rainforest.py
+++ rainforest/rainforest.py
@@ -1,10 +1,36 @@
 # -*- coding: utf-8 -*-
 
-# keras cnn
+# rainforest
 
+import matplotlib
+matplotlib.use('TkAgg')
+
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import tensorflow as tf
+
+
+import sys
+import os
+import subprocess
+
+from six import string_types
+
+# Make sure you have all of these packages installed, e.g. via pip
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
-import tensorflow as tf
+import matplotlib.image as mpimg
+import scipy
+from skimage import io
+from scipy import ndimage
+# from IPython.display import display
+# %matplotlib inline
+
+
+labels_df = pd.read_csv("data/train.csv")
+print(labels_df.head())
 
 
